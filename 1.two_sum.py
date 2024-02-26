5388a0b1b3a90cf2d49fee3f920d2f1e68589207
wessel.blokzijl <wessel.blokzijl@huntandhackett.com>
2024-02-26 10:19:08 +0100

    tm

--- 1.two_sum.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        lookup = set(nums)
-        return [0,1]
-        for index, num in enumerate(nums):
-            if target - num in lookup:
-                return [index, num.index(target-num, 0, index-1) or num.index(target-num, index +1)]
